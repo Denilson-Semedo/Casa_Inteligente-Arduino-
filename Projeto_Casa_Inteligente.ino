d723a624d0c2cabc48de4fb0cef3e20479d46efb
Denilson Semedo Tavares <55548914+Denilson-Semedo@users.noreply.github.com>
2019-12-18 17:07:28 -0100

    final version

--- Projeto_Casa_Inteligente.ino
+++ Projeto_Casa_Inteligente.ino
@@ -1,163 +1,154 @@
 #include <Servo.h>
 Servo servo;
-int red_light_pin= 11;
-int green_light_pin = 10;
-int blue_light_pin = 9;
-
-char op;
-char opcao;
+Servo servo1;
 float temperatura;
 
-void setup()
-{
-  pinMode(10,OUTPUT);
-  digitalWrite(10,LOW);
-  pinMode(13, OUTPUT);
-  servo.attach(8);
-  servo.write(0);
-  pinMode(red_light_pin, OUTPUT);
-  pinMode(green_light_pin, OUTPUT);
-  pinMode(blue_light_pin, OUTPUT);
+void setup() {
   pinMode(2,OUTPUT);
   pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
+  pinMode(7,INPUT);
+  pinMode(8,OUTPUT);
   pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(14,OUTPUT);
+  pinMode(16,OUTPUT);
+  pinMode(13,OUTPUT);
+  digitalWrite(13,LOW); 
+  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
+
+  servo.attach(15);
+  servo.write(0);
+  servo1.attach(12);
+  servo1.write(0);
   Serial.begin(9600);
 }
-
-void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
- {
-  analogWrite(red_light_pin, red_light_value);
-  analogWrite(green_light_pin, green_light_value);
-  analogWrite(blue_light_pin, blue_light_value);
-}
-
-void ledrgb(){
-  RGB_color(255, 0, 0); 
-  delay(80);
-  RGB_color(0, 255, 0); 
-  delay(80);
-  RGB_color(0, 0, 255);
-  delay(80);
-  RGB_color(255, 255, 125); 
-  delay(80);
-  RGB_color(0, 255, 255);
-  delay(80);
-  RGB_color(255, 0, 255);
-  delay(80);
-  RGB_color(255, 255, 0);
-  delay(80);
-  RGB_color(255, 255, 255);
-  delay(80);
-}
-
-
-void porta(){
-    // put your main code here, to run repeatedly:
+int x=0;
+int y=0;
+void portaopen(){
     int i;
     for(i=0;i<90;i++){
-      servo.write(i);
-      delay(10);
-    }
-  
-    delay(6000);
-    
+    servo.write(i);
+    delay(10);
+    } 
+    x=1;
+}
+void portaclose(){
+    int i;
     for(i=90;i>0;i--){
-      servo.write(i);
-      delay(10);
-    }
-     delay(6000);
+    servo.write(i);
+    delay(10);
+    } 
+    x=0;
+}
+void garaopen(){
+    int i;
+    for(i=0;i<90;i++){
+    servo1.write(i);
+    delay(10);
+    } 
+    y=1;
+}
+void garaclose(){
+    int i;
+    for(i=90;i>0;i--){
+    servo1.write(i);
+    delay(10);
+    } 
+    y=0;
 }
 
-void LuzDaCasa(){
-  Serial.println("1->quarto 1");
-  Serial.println("2->quarto 2");
-  Serial.println("3->quarto 3");
-  Serial.println("4->quarto 4");
-  Serial.println("5->Apagar todas luzes");
+void loop() {
+  noTone(14);
+  temperatura = (float(analogRead(A0))*5/(1023))/0.01;
+  Serial.println(temperatura);
+  if(temperatura > 36){
+    digitalWrite(13,HIGH);
+  }
 
-  int op;
+  if(digitalRead(7)==HIGH){
+    tone(14,1000);  
+    noTone(14);
+  }
   
-  op = (Serial.read());
-
-  switch(op)
-  {
-    case '1':
-      digitalWrite(2,HIGH);    
-      break;
-    case '2':
-      digitalWrite(3,HIGH);
-      break;
-    case '3':
+    char c;
+    c = (Serial.read());
+    
+    if(c == 'A'){
+      digitalWrite(9,HIGH);  
+    } 
+    if(c == 'B'){
+       digitalWrite(9,LOW);
+    }
+    if(c == 'C' && x==0){
+      portaopen(); 
+    }
+    if(c == 'D' && x==1){
+      portaclose();
+    }
+    if(c == 'Z' && y==0){
+      garaopen(); 
+    }
+    if(c == 'Y' && y==1){
+      garaclose();
+    }
+    if(c == 'W'){
+      digitalWrite(16,HIGH);
+    }
+    if(c == 'T'){
+      digitalWrite(16,LOW);
+    }
+    if(c == 'E'){
+       digitalWrite(13,HIGH);
+    }
+    if(c == 'F'){
+       digitalWrite(13,LOW);
+    }
+    if(c == 'G'){
+       digitalWrite(6,HIGH);
+    }
+    if(c == 'H'){
+       digitalWrite(6,LOW);
+    }
+    if(c == 'I'){
+      digitalWrite(2,HIGH); 
+    }
+    if(c == 'J'){
+       digitalWrite(2,LOW);
+    }
+    if(c == 'K'){
+      digitalWrite(3,HIGH); 
+    }
+    if(c == 'L'){
+       digitalWrite(3,LOW);
+    }
+    if(c == 'M'){
+       digitalWrite(8,HIGH);
+    }
+    if(c == 'N'){
+       digitalWrite(8,LOW);
+    }
+    if(c == 'O'){
       digitalWrite(4,HIGH);
-      break;
-    case '4':
+    }
+    if(c == 'P'){
+       digitalWrite(4,LOW);
+    }
+    if(c == 'R'){
       digitalWrite(5,HIGH);
-      break;
-    case '5':
+    }
+    if(c == 'S'){
+       digitalWrite(5,LOW);
+    }
+    if(c == 'Q'){
       digitalWrite(2,LOW);
       digitalWrite(3,LOW);
       digitalWrite(4,LOW);
       digitalWrite(5,LOW);
-      break;
-    default:
-      break;
-  }
-}
-
-void loop()
-{
-  temperatura = (float(analogRead(A0))*5/(1023))/0.01;
-  if(temperatura > 40){
-    digitalWrite(10,HIGH);
-  }
-  if (Serial.available())
-
-  {
-    op = (Serial.read());
-
-    switch(op){
-        Serial.println("switch1");
-        case '1':
-         //AC();
-         digitalWrite(13, HIGH);
-         Serial.println("\t\t\t\t\t\t\t\tCASA INTELIGENTE");
-         Serial.println("1->Ligar AC;");
-         Serial.println("2->Acender a luz;");
-         Serial.println("3->Acender a luz da piscina;");
-         Serial.println("4->Abrir a porta principal;");
-         Serial.println("5->Abrir a porta da garagem;");
-        
-         opcao = (Serial.read());
-
-         switch(opcao){
-              Serial.println("switch2");
-              case '1':
-               LuzDaCasa();
-               Serial.println(opcao);
-               break;
-              case '2':
-               //LuzDaCasa();
-               digitalWrite(13, LOW);
-               Serial.println(opcao);
-               break;
-              case '3':
-               ledrgb();
-               break;
-              case '4':
-               porta();
-               Serial.println(opcao);
-               break;
-              case '5':
-               //Garagem();
-               break;
-              default:
-                break;
-              }         
-             
-             break;
-            default:
-              break;
-      }
-  }
+      digitalWrite(8,LOW);
+      digitalWrite(6,LOW);
+      digitalWrite(9,LOW);
+    }
 }
